e2197f46b881ec46dd5c3d85af748293be8c2114
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2024-11-12 15:00:14 +0100

    do exercises

--- app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
@@ -6,18 +6,60 @@ class LocalStudentManager : StudentManager {
     // TODO 2: init the class with a list of 10 students every time a new instance is created;
     // Use the followi
 
-    /**
-     * listOf(
-    Etudiant(name = "John1", code = "A1", sexe = "M", address = "Lilles", age = 20),
-    Etudiant(name = "John2", code = "A2", sexe = "F", address = "Lilles", age = 30),
-    Etudiant(name = "John3", code = "A3", sexe = "F", address = "Lilles", age = 41),
-    Etudiant(name = "John4", code = "A4", sexe = "M", address = "Lilles", age = 42),
-    Etudiant(name = "John5", code = "A5", sexe = "M", address = "Lilles", age = 34),
-    Etudiant(name = "John6", code = "A6", sexe = "F", address = "Lilles", age = 12),
-    Etudiant(name = "John7", code = "A7", sexe = "M", address = "Lilles", age = 28),
-    Etudiant(name = "John8", code = "A8", sexe = "M", address = "Lilles", age = 16),
-    Etudiant(name = "John9", code = "A9", sexe = "F", address = "Lilles", age = 10),
-    Etudiant(name = "John10", code = "A10", sexe = "F", address = "Lilles", age = 56),
+    var students = listOf(
+        Student(name = "John1", code = "A1", sexe = "M", address = "Lilles", age = 20),
+        Student(name = "John2", code = "A2", sexe = "F", address = "Lilles", age = 30),
+        Student(name = "John3", code = "A3", sexe = "F", address = "Lilles", age = 41),
+        Student(name = "John4", code = "A4", sexe = "M", address = "Lilles", age = 42),
+        Student(name = "John5", code = "A5", sexe = "M", address = "Lilles", age = 34),
+        Student(name = "John6", code = "A6", sexe = "F", address = "Lilles", age = 12),
+        Student(name = "John7", code = "A7", sexe = "M", address = "Lilles", age = 28),
+        Student(name = "John8", code = "A8", sexe = "M", address = "Lilles", age = 16),
+        Student(name = "John9", code = "A9", sexe = "F", address = "Lilles", age = 10),
+        Student(name = "John10", code = "A10", sexe = "F", address = "Lilles", age = 56),
     )
-     */
+
+    override fun listOf10(): List<Student> {
+        return students
+    }
+
+    override fun sortAgeAscDesc(type: String): List<Student> {
+        when (type.lowercase()) {
+            "asc" -> return this.listOf10().sortedBy { it.age }
+            "desc" -> return this.listOf10().sortedByDescending { it.age }
+            else -> {
+                println("The type '$type' is incorrect")
+                return this.listOf10()
+            }
+        }
+    }
+
+    override fun groupBySex(): Map<String, List<Student>> {
+        return this.listOf10().groupBy { it.sexe }
+    }
+
+    override fun boysOrGirls(sexe: String): List<Student> {
+        val availableSexe = listOf("M", "F")
+        if (sexe.uppercase() !in availableSexe){
+            println("The sexe '$sexe' is incorrect")
+            return this.listOf10()
+        }
+        return this.listOf10().filter { it.sexe == sexe.uppercase() }
+    }
+
+    override fun onlyNames(): List<String> {
+        return this.listOf10().map { it.name }
+    }
+
+    override fun deleteBySex(sex: String) {
+        students = this.listOf10().filter { it.sexe != sex.uppercase() }
+    }
+
+    override fun reverse(): List<Student> {
+        return this.listOf10().reversed()
+    }
+
+    override fun clear() {
+        students = this.listOf10().filter { false }
+    }
 }
